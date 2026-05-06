b93d18839bc53d6d728dedbaee98250dd7456e41
parthasarathyg695-gif <parthasarathyg695@gmail.com>
2026-05-07 01:20:08 +0530

    Remove frontend folder (moved to separate repository)

--- gyanteerth/src/pages/trainer/Curriculum.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CoursePlayer from '../student/CoursePlayer';
-
-const TrainerCurriculum = () => {
-  return <CoursePlayer isTrainer={true} />;
-};
-
-export default TrainerCurriculum;
--- gyanteerth/src/shared/components/Loader.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loader() {
-  return (
-    <div className="flex justify-center items-center h-full">
-      <p>Loading...</p>
-    </div>
-  );
-}
