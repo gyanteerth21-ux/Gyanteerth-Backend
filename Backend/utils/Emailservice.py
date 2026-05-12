6858ddd01ac064571abad5982ea83746993099c4
parthasarathyg695-gif <parthasarathyg695@gmail.com>
2026-05-12 20:59:08 +0530

    Secure email credentials and prepare for final push

--- Backend/utils/Emailservice.py
+++ Backend/utils/Emailservice.py
@@ -1,14 +1,20 @@
 import aiosmtplib
+import os
 from email.message import EmailMessage
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 async def send_email(to_email: str, subject: str, body: str):
     message = EmailMessage()
-    message["From"] = "aruljayarajj826@gmail.com"
+    sender_email = os.getenv("EMAIL_USER")
+    sender_password = os.getenv("EMAIL_PASSWORD")
+    
+    message["From"] = sender_email
     message["To"] = to_email
     message["Subject"] = subject
 
     message.set_content("Your email client does not support HTML.")
-
     message.add_alternative(body, subtype="html")
 
     await aiosmtplib.send(
@@ -16,6 +22,6 @@ async def send_email(to_email: str, subject: str, body: str):
         hostname="smtp.gmail.com",
         port=587,
         start_tls=True,
-        username="aruljayarajj826@gmail.com",
-        password="ysbqpkpkfmkypfub" 
+        username=sender_email,
+        password=sender_password
     )
